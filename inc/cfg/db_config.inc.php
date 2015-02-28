0e8cb93914ee549d4e05e99da219b1d32627064f
Dan <dan@pixelspace.org>
2015-02-28 23:36:31 +0100

    config file for database

---
+++ inc/cfg/db_config.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('table_users', 'users');
+define('table_sessions', 'sessions');
+define('table_categories', 'categories');
+define('table_fields', 'fields');
+define('table_survey', 'survey');
+define('table_feedback', 'feedback');
+define('table_news', 'news');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'db_name');
+define('DB_USER', 'db_user');
+define('DB_PASS', 'db_pass');
+?>
